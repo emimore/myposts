32a3ed110a6ef34fe64352ca39fb35449bab5809
emimore <44500382+emimore@users.noreply.github.com>
2021-03-29 14:48:37 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,12 @@
 <?php
 
 	//Criar a conexão
-	$conexao = mysqli_connect("localhost", "root", "root", "renovigi") or die ("Erro");
+	$conexao = mysqli_connect("localhost", "root", "", "") or die ("Erro");
 	mysqli_query($conexao,"set names 'utf8'");
 	
 	function cadastrar($nome_cad,$email_cad,$men_cad){
             $nome_cad=($nome_cad);
-            $sql = "SELECT nome_cad FROM usuario");
+            $sql = "SELECT nome_cad FROM usuario";
             $resultado = mysqli_query($conexao, $sql);
             if($resultado !== $nome_usuario ){
 				$sql = $resultado->prepare("INSERT INTO usuario (nome_cad,email_cad,men_cad) VALUES ('$nome_cad','$email_cad,'$men_cad')");

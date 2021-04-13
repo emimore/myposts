7d254c9c959f18e76406bc478c4da6bfc1818b30
emimore <44500382+emimore@users.noreply.github.com>
2021-04-12 21:50:19 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,10 @@
 <?php
 
-	//Criar a conexão
 	$conexao = mysqli_connect("localhost", "root", "", "") or die ("Erro");
 	mysqli_query($conexao,"set names 'utf8'");
+	 $nome_cad = $_POST ["nome_cad"];
+  	$email_cad = $_POST ["email_cad"];
+	$men_cad = $_POST ["men_cad"];	
 	
 	function cadastrar($nome_cad,$email_cad,$men_cad){
             $nome_cad=($nome_cad);
